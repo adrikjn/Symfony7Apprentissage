27aae9fbe865ad5c68bc75663bba74b503ecc8b5
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2024-04-24 14:16:48 +0200

    Création Projet Tuto Symfony 7 & Controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route("/", name: "home")]
+    function index (Request $request): Response {
+        dd($request);
+        return new Response('Bonjour ' . $request->query->get('name') );
+    }
+}
