0dd4e083fd545ac60e639a60902d9884c84371b7
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2024-04-25 15:01:58 +0200

    Création BDD/entité/Manipulation du Repository Création de query builder/ Affichage dans le twig : boucle.. redirection..

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,16 +10,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recettes', name: 'recipe.index')]
-    public function index(Request $request): Response {
-       return $this->render('recipe/index.html.twig');
-    }
 
-    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements:['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response {
-       return $this->render('recipe/show.html.twig', [
-         'slug' => $slug,
-         'id' => $id
-       ]);
-    }
+   #[Route('/recettes', name: 'recipe.index')]
+   public function index(Request $request, RecipeRepository $repository): Response
+   {
+      $recipes = $repository->findWithDurationLowerThan(20);
+      return $this->render('recipe/index.html.twig',[
+         'recipes' => $recipes
+      ]);
+   }
+
+   #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+   public function show(Request $request, string $slug, int $id, RecipeRepository $repo): Response
+   {
+      $recipe = $repo->find($id);
+      if($recipe->getSlug() !== $slug) {
+         return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
+      }
+      return $this->render('recipe/show.html.twig', [
+         'recipe' => $recipe
+      ]);
+   }
 }
