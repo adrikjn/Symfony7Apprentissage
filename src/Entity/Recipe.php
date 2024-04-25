b96af76234ffd1fcabe525d99525fd6f1773e4e3
[Adrien Kouyoumjian] <[adrien.kouyoumjian@outlook.fr]>
2024-04-25 18:43:32 +0200

    Valider les données de formulaire (Entité ou TypeForm)/ Gérer les banword

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -3,10 +3,16 @@
 namespace App\Entity;
 
 use App\Repository\RecipeRepository;
+use App\Validator\BanWord;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Positive;
 
 #[ORM\Entity(repositoryClass: RecipeRepository::class)]
+#[UniqueEntity('title')]
+#[UniqueEntity('slug')]
 class Recipe
 {
     #[ORM\Id]
@@ -15,12 +21,19 @@ class Recipe
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(min: 5)]
+    #[BanWord()]
+    #[Assert\Length(min: 5)]
+
     private ?string $title = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Regex('/^[a-z0-9]+(?:-[a-z0-9]+)*$/')]
+    #[Assert\Length(min: 5)]
     private ?string $slug = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\Length(min: 5)]
     private ?string $content = null;
 
     #[ORM\Column]
@@ -30,6 +43,8 @@ class Recipe
     private ?\DateTimeImmutable $updatedAt = null;
 
     #[ORM\Column(nullable: true)]
+    #[Assert\Positive()]
+    #[Assert\LessThan(value:1440)]
     private ?int $duration = null;
 
     public function getId(): ?int
--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -11,30 +11,33 @@ use Symfony\Component\String\Slugger\AsciiSlugger;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Sequentially;
 
 class RecipeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class, [
-                'label' => 'Titre'
+            ->add('title')
+            ->add('slug', TextType::class, [
+                'required' => false,
             ])
-            ->add('slug')
             ->add('content')
-            ->add('duration')
-            ->addEventListener(FormEvents::PRE_SUBMIT, $this->autoSlug(...))
-        ;
+            ->add('duration');
+            // ->addEventListener(FormEvents::PRE_SUBMIT, $this->autoSlug(...));
     }
 
-    public function autoSlug(PreSubmitEvent $event):void{
-        $data = $event->getData();
-        if(empty($data["slug"])){
-            $slugger = new AsciiSlugger();
-            $data['slug'] = strtolower($slugger->slug($data['title']));
-        }
-        $event->setData($data);
-    }
+    // public function autoSlug(PreSubmitEvent $event): void
+    // {
+    //     $data = $event->getData();
+    //     if (empty($data["slug"])) {
+    //         $slugger = new AsciiSlugger();
+    //         $data['slug'] = strtolower($slugger->slug($data['title']));
+    //     }
+    //     $event->setData($data);
+    // }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
